231014c3bc1802620954b8fa2395a4e33eb4cd87
Vanh1012 <23127321@student.hcmus.edu.vn>
2026-05-04 11:54:20 +0700

    fix: wrap optional task imports to avoid missing package errors

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,18 @@
 from .arc import AI2ArcTask
 from .base import FewShotTask, Task
-from .gsm8k import Gsm8kTask
 from .hellaswag import HellaSwagTask
 from .math import MathTask
-from .mbpp2 import Mbpp2Task
 from .mmlu import MMLUTask
+
+try:
+    from .gsm8k import Gsm8kTask
+except ImportError:
+    pass
+
+try:
+    from .mbpp2 import Mbpp2Task
+except ImportError:
+    pass
 
 try:
     from .cls import ClsTask
