2e21eedb59d78fff26161114a3d551bd5b59dbcb
Vanh1012 <23127321@student.hcmus.edu.vn>
2026-05-04 11:52:58 +0700

    fix: lazy import ClsTask to avoid evalplus version conflict

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,8 +1,12 @@
 from .arc import AI2ArcTask
 from .base import FewShotTask, Task
-from .cls import ClsTask
 from .gsm8k import Gsm8kTask
 from .hellaswag import HellaSwagTask
 from .math import MathTask
 from .mbpp2 import Mbpp2Task
 from .mmlu import MMLUTask
+
+try:
+    from .cls import ClsTask
+except ImportError:
+    pass
